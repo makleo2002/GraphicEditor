15b3681197cd4c0040bca6bbe94e9169a34de780
makleo2002 <75272208+makleo2002@users.noreply.github.com>
2022-06-09 18:48:54 +0300

    Add files via upload

---
+++ TextEdit/TextEdit/resource.h
@@ -0,0 +1,47 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется TextEdit.rc
+#define IDR_APPMENU                     102
+#define IDI_APPICON                     103
+#define IDI_APPICONSM                   104
+#define ID_FILE_EXIT                    40001
+#define ID_HELP_ABOUT                   40003
+#define ID_FORMAT_BOLD                  40010
+#define ID_FORMAT_ITALIC                40011
+#define ID_FORMAT_UNDERLINE             40012
+#define ID_FORMAT_FONT                  40013
+#define ID_FORMAT_PARAGRAPH_LEFT        40014
+#define ID_FORMAT_PARAGRAPH_RIGHT       40015
+#define ID_FORMAT_PARAGRAPH_CENTER      40016
+#define ID_EDIT_UNDO                    40018
+#define ID_EDIT_CUT                     40019
+#define ID_EDIT_COPY                    40020
+#define ID_EDIT_PASTE                   40021
+#define ID_EDIT_DELETE                  40022
+#define ID_FILE_SAVEAS                  40024
+#define ID_EDIT_SELECTALL               40028
+#define ID_FILE_NEW                     0xE100
+#define ID_FILE_OPEN                    0xE101
+#define ID_FILE_PRINT                   0xE107
+#define IDM_EXIT                        105
+#define IDM_ABOUT                       104
+#define IDR_MAINFRAME                   128
+#define IDI_TEXTEDIT                    107
+#define IDC_TEXTEDIT                    109
+#define IDC_STATIC                      -1
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX			        103
+#define IDC_MYICON				        2
+#define IDI_SMALL				        108
+#define IDD_TEXTEDIT_DIALOG	            102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        121
+#define _APS_NEXT_COMMAND_VALUE         40029
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ TextEdit/TextEdit/rtfpad.h
@@ -0,0 +1,23 @@
+// -----------------------------------------------------
+// Описание функций
+// -----------------------------------------------------
+LRESULT WINAPI
+WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL WndProc_OnCreate(HWND hWnd,
+	LPCREATESTRUCT lpCreateStruct);
+void WndProc_OnDestroy(HWND hWnd);
+void WndProc_OnCommand(HWND hWnd, int id,
+	HWND hwndCtl, UINT codeNotify);
+LRESULT WndProc_OnNotify(HWND hWnd, int idFrom,
+	NMHDR FAR* pnmhdr);
+void WndProc_OnSize(HWND hwnd, UINT state, int cx, int cy);
+void WndProc_OnSetFocus(HWND hwnd, HWND hwndOldFocus);
+void FileSaveAs(HWND hwnd);
+DWORD CALLBACK SaveCallback(DWORD dwCookie, LPBYTE  pbBuff,
+	LONG cb, LONG* pcb);
+void FileOpen(HWND hwnd);
+DWORD CALLBACK
+OpenCallback(DWORD dwCookie, LPBYTE   pbBuff,
+	LONG cb, LONG* pcb);
+void FilePrint(void);
+#define IDC_RTFEDIT 1236
